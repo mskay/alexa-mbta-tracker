8caf5342a9dbcede31bcb2a006ed03a2b2dad6b2
Matthew <mskay@carbonblack.com>
2018-11-30 10:10:45 -0500

    Add support for some Green line stations

--- src/stations.py
+++ src/stations.py
@@ -1,55 +1,106 @@
 stations = {
     'red': {
         'jfk/umass': {
-            'outbound': 70085,
-            'inbound': 70086
+            'ashmont': 70085,
+            'braintree': 70085,
+            'alewife': 70086
         },
         'andrew': {
-            'outbound': 70083,
-            'inbound': 70084
+            'ashmont': 70083,
+            'braintree': 70083,
+            'alewife': 70084
         },
         'broadway': {
-            'outbound': 70081,
-            'inbound': 70082
+            'ashmont': 70081,
+            'braintree': 70081,
+            'alewife': 70082
         },
         'south station': {
-            'outbound': 70079,
-            'inbound': 70080
+            'ashmont': 70079,
+            'braintree': 70079,
+            'alewife': 70080
         },
         'downtown crossing': {
-            'outbound': 70077,
-            'inbound': 70078
+            'ashmont': 70077,
+            'braintree': 70077,
+            'alewife': 70078
         },
         'park street': {
-            'outbound': 70075,
-            'inbound': 70076
+            'ashmont': 70075,
+            'braintree': 70075,
+            'alewife': 70076
         },
         'charles/mgh': {
-            'outbound': 70073,
-            'inbound': 70074
+            'ashmont': 70073,
+            'braintree': 70073,
+            'alewife': 70074
         },
         'kendall/mit': {
-            'outbound': 70071,
-            'inbound': 70072
+            'ashmont': 70071,
+            'braintree': 70071,
+            'alewife': 70072
         },
         'central': {
-            'outbound': 70069,
-            'inbound': 70070
+            'ashmont': 70069,
+            'braintree': 70069,
+            'alewife': 70070
         },
         'harvard': {
-            'outbound': 70067,
-            'inbound': 70068
+            'ashmont': 70067,
+            'braintree': 70067,
+            'alewife': 70068
         },
         'porter': {
-            'outbound': 70065,
-            'inbound': 70066
+            'ashmont': 70065,
+            'braintree': 70065,
+            'alewife': 70066
         },
         'davis': {
-            'outbound': 70063,
-            'inbound': 70064
+            'ashmont': 70063,
+            'braintree': 70063,
+            'alewife': 70064
         },
         'alewife': {
-            'outbound': 70061,
+                'ashmont': 70061,
+        }
+    },
+    'green': {
+        'boylston': {
+            'copley': 70159,
+            'west': 70159,
+            'park street': 70158,
+            'north station': 70158,
+        },
+        'arlington': {
+            'copley': 70157,
+            'west': 70157,
+            'park street': 70156,
+            'north station': 70156,
+        },
+        'copley': {
+            'kenmore': 70155,
+            'west': 70155,
+            'heath street': 70155,
+            'park street': 70154,
+            'north station': 70154,
+        },
+        'hynes convention center': {
+            'kenmore': 70153,
+            'west': 70153,
+            'park street': 70152,
+            'north station': 70152,
+        },
+        'kenmore': {
+            'boston college': 70151,
+            'cleveland circle': 70151,
+            'riverside': 70151,
+            'park street': 70150,
+            'north station': 70150
+        },
+        'coolidge corner': {
+            'cleveland circle': 70219,
+            'park street': 70220,
+            'north station': 70220
         }
     }
 }
